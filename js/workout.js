25c6c492af8673239f9c297e3bb9e2c5b3aad528
macmaz <maciej.mazurek87@gmail.com>
2017-08-20 16:17:51 +0200

    workouts

---
+++ js/workout.js
@@ -0,0 +1,29 @@
+//Zadanie 1
+
+function ustawTlo(){
+
+document.getElementById("one").style.backgroundColor = "red";
+
+document.getElementById("two").style.backgroundColor = "yellow";
+
+}
+
+document.getElementById('background').onclick = ustawTlo;
+
+// Zadanie 2
+
+function pobierzImie(event) {
+    
+    event.preventDefault();
+    
+    var inputs = document.getElementsByTagName('input');
+    var i = 0;
+    while ( i < inputs.length){
+        if(inputs[i].type == 'text'){
+        console.log(inputs[i].value);
+        }
+        i++;
+    }
+}
+
+// Zadanie 3
